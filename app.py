4bc1bef5e86b6b7016128f1137dfcf7f419be469
charlie2423 <charlie22222222222@gmail.com>
2025-07-26 14:19:29 -0700

    Update app.py

--- app.py
+++ app.py
@@ -3,13 +3,22 @@ import plotly.express as px
 import streamlit as st
      
 car_data = pd.read_csv('vehicles_us.csv') # leer los datos
+st.header('Análisis Exploratorio de Datos (EDA) de Vehículos') # título de la aplicación
 hist_button = st.button('Construir histograma') # crear un botón
+
 if hist_button: # al hacer clic en el botón
          # escribir un mensaje
-    st.write('Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
+     st.write('Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
          
          # crear un histograma
-    fig = px.histogram(car_data, x="odometer")
+     fig = px.histogram(car_data, x="odometer")
      
          # mostrar un gráfico Plotly interactivo
-    st.plotly_chart(fig, use_container_width=True)
+     st.plotly_chart(fig, use_container_width=True)
+import streamlit as st
+
+# crear una casilla de verificación
+build_histogram = st.checkbox('Construir un histograma')
+
+if build_histogram: # si la casilla de verificación está seleccionada
+    st.write('Construir un histograma para la columna odómetro')
